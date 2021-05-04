2525838adf1340829d4818b7ca090ada3f271219
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-05-04 19:47:33 +0900

    Revert "Issue4 2"
    This reverts commit 0d9028f0e1a27b6b34308d89fe116388af8faa2e.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def getWeek
@@ -27,20 +27,14 @@ class CalendarsController < ApplicationController
 
     @week_days = []
 
-    plans = .where(date: @todays_date..@todays_date + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.eday  # wdayメソッドを用いて取得した数値
-      if #「wday_numが7以上の場合」という条件式
-        wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans :wdays => @week_days}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
 
