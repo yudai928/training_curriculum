d2a005d6b0c2a2828f1056e6f530ea2dda350077
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-05-05 14:05:06 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -28,15 +28,28 @@ class CalendarsController < ApplicationController
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
+    # 今日から1週間 = plans
+
+    #     # 配列plansの中身はこのような状態。
+#     # [#<Plan:0x00007fdf5c18b378 id: 14, plan: "遊びに行く", date: Sun, 17 Jan 2021, created_at: Tue, 12 Jan 2021 15:01:37 UTC +00:00, updated_at: Tue, 12 Jan 2021 15:01:37 UTC +00:00>,
+#     #<Plan:0x00007fdf5c91cb50 id: 16, plan: "やっぱりいかない", date: Sun, 17 Jan 2021, created_at: Tue, 12 Jan 2021 15:11:53 UTC +00:00, updated_at: Tue, 12 Jan 2021 15:11:53 UTC +00:00>,
+#     #<Plan:0x00007fdf5c91ca88 id: 17, plan: "この日に遊ぶ", date: Mon, 18 Jan 2021, created_at: Tue, 12 Jan 2021 15:12:05 UTC +00:00, updated_at: Tue, 12 Jan 2021 15:12:05 UTC +00:00>]
 
     7.times do |x|
-      today_plans = []
+      today_plans = [] #7回eachされた事で、1週間分の予定が入っている
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = today_plans[x]
+      if wday_num.to_i >= 7
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[x]}
+      # daysは月、日にち、曜日が1週間分入っている
       @week_days.push(days)
     end
-
   end
-end
+  end
+
